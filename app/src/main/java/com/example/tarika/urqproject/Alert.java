ed51b6a17a8e6be71924c05d4b6852ee997ca9f3
iamtarika <pairsir@outlook.com>
2017-09-12 20:23:32 +0700

    Alert XML

--- app/src/main/java/com/example/tarika/urqproject/Alert.java
+++ app/src/main/java/com/example/tarika/urqproject/Alert.java
@@ -3,7 +3,7 @@ package com.example.tarika.urqproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Alert extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
