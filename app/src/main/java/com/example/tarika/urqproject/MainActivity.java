08a4d45dad010fb909c27fe9e568e0c664e995aa
iamtarika <pairsir@outlook.com>
2017-09-12 21:55:49 +0700

    Add styles

--- app/src/main/java/com/example/tarika/urqproject/MainActivity.java
+++ app/src/main/java/com/example/tarika/urqproject/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.tarika.urqproject;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
